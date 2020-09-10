80bcf0d05fcf5232704360a4e6d61b6f1ff3b1e5
Devon LaFrance <lafrance.devon@gmail.com>
2020-09-10 10:40:45 -0600

    Updates

---
+++ precache-manifest.75e7c845b7a2434caa6f90e1f28330ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb5b80243bc3fb61e920fb26ea76d5b",
+    "url": "/XmasCountdownWeb/index.html"
+  },
+  {
+    "revision": "b10d2f019ac030746488",
+    "url": "/XmasCountdownWeb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b4db97cbc318b66e7a8",
+    "url": "/XmasCountdownWeb/static/js/2.1b41005e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/XmasCountdownWeb/static/js/2.1b41005e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10d2f019ac030746488",
+    "url": "/XmasCountdownWeb/static/js/main.76f740e5.chunk.js"
+  },
+  {
+    "revision": "59246d4fd5c50fe40bc5",
+    "url": "/XmasCountdownWeb/static/js/runtime-main.d2bdfab1.js"
+  },
+  {
+    "revision": "f5f1999360b3f91fdd3802f38bea550d",
+    "url": "/XmasCountdownWeb/static/media/1_XmasWebHome.f5f19993.png"
+  },
+  {
+    "revision": "133b6e459e846e1aa0c5d81751a55b7b",
+    "url": "/XmasCountdownWeb/static/media/2_XmasWebWeeks.133b6e45.png"
+  },
+  {
+    "revision": "24ce97449dfd1bae74f024105603b8fa",
+    "url": "/XmasCountdownWeb/static/media/3_XmasWebDays.24ce9744.png"
+  },
+  {
+    "revision": "381a55fd068dc8c81fa14e42489c279d",
+    "url": "/XmasCountdownWeb/static/media/4_XmasWebHours.381a55fd.png"
+  },
+  {
+    "revision": "13ea8baffe4867d772eb2a90c183ffdb",
+    "url": "/XmasCountdownWeb/static/media/5_XmasWebMinutes.13ea8baf.png"
+  },
+  {
+    "revision": "048ba197669dd2390f1cef40b97eac7b",
+    "url": "/XmasCountdownWeb/static/media/6_XmasWebSeconds.048ba197.png"
+  }
+]);
